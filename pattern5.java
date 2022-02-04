33f5cb6355f3c3aeb2ec3bbf4f166d0ec070d89f
Chaitanya More <98660677+Chaitanya-more@users.noreply.github.com>
2022-02-04 21:04:51 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,16 @@
+package pattern5;
+
+public class pattern5 {
+	public static void main(String arsg [])
+	{
+		for(int i=1; i<=4;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
